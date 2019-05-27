c4809df64f917cbbc0dd2d9bd72dd64295c92fca
Heather deManbey <h.demanbey@gmail.com>
2019-05-27 14:28:46 -0400

    made projects model, controller and routes

---
+++ db/migrate/20190527182343_create_projects.rb
@@ -0,0 +1,17 @@
+class CreateProjects < ActiveRecord::Migration[5.2]
+  def change
+    create_table :projects do |t|
+      t.string :name
+      t.text :short_description
+      t.text :long_description
+      t.string :url
+      t.string :location
+      t.string :organization
+      t.text :org_description
+      t.boolean :active
+      t.string :photo
+
+      t.timestamps
+    end
+  end
+end
