51dbd97d112360ef2059f6bb1ab11019b9d3bad7
Heather deManbey <h.demanbey@gmail.com>
2019-05-29 14:11:19 -0400

    added a create/destroy toggle to favorites and on profile

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,14 +1,26 @@
 class FavoritesController < ApplicationController
-before_action :setFavorite
+before_action :setFavorite, only: %i[destroy]
 
   def index
-    # if current_user != null
+    # if current_user != nil
     #   @user = current_user
     #   @favorites = Favorite.all.where(...)
     # end
   end
 
+  def toggle
+    @project = Project.find(params[:project_id])
 
+    if current_user.has_favorited?(@project)
+      @favorite = Favorite.find_by(user_id: current_user.id, project_id: params[:project_id])
+      @favorite.destroy
+    else
+      @favorite = Favorite.new
+      @favorite.user_id = current_user.id
+      @favorite.project_id = params[:project_id]
+      @favorite.save
+    end
+  end
 
   def setFavorite
     @favorite = Favorite.find(params[:id])
