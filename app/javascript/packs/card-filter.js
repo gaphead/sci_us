8c242f18e6d7b27e536eb9dc81d46d99f9b31b7f
Erik Pendleton <erik.codigo@gmail.com>
2019-05-30 16:32:19 -0400

    fixed the icon sorting feature

--- app/javascript/packs/card-filter.js
+++ app/javascript/packs/card-filter.js
@@ -1,12 +1,16 @@
 const filter = () => {
   const id = document.querySelectorAll(".filter")
+  const cards = document.querySelectorAll(".card")
+
   id.forEach(icon => icon.addEventListener("click", () => {
     // add class list to toggle color of icon id.classList.toggle("active")
     const category = icon.getAttribute("title").toLowerCase();
     console.log(category)
-    const cards = document.querySelectorAll(".card")
     cards.forEach(card => {
-      card.classList.contains(category) ? card.classList.remove("hide") : card.classList.add("hide")
+      card.classList.contains(category) ? card.classList.remove("hide") : card.classList.add("hide");
+      // if(card.classList.contains(category)) {
+      //   card.classList.toggle("hide")
+      // }
 
     })
   }))
