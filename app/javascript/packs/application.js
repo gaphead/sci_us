18b9e4345567c5da5198bae14b2c881fa59eb668
Erik Pendleton <erik.codigo@gmail.com>
2019-05-31 15:33:18 -0400

    fixed js bug and changed profile picture

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,13 +2,13 @@ import "bootstrap";
 import filter from "./card-filter.js";
 filter();
 
-$(function() {
-   $(window).scroll(function () {
-      if ($(this).scrollTop() > 300) {
-         $(‘body’).addClass(‘changeColor’)
-      }
-      if ($(this).scrollTop() < 300) {
-         $(‘body’).removeClass(‘changeColor’)
-      }
-   });
-});
+// $(function() {
+//    $(window).scroll(function () {
+//       if ($(this).scrollTop() > 300) {
+//          $('body').addClass('changeColor')
+//       }
+//       if ($(this).scrollTop() < 300) {
+//          $('body').removeClass('changeColor')
+//       }
+//    });
+// });
