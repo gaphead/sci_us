7e3f08d8342a15a63253f1cebfa3c76e0b3f2768
Alex Tang <altg514@gmail.com>
2019-06-04 13:12:29 -0400

    Test

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_04_164922) do
+ActiveRecord::Schema.define(version: 2019_06_04_170937) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -50,7 +50,6 @@ ActiveRecord::Schema.define(version: 2019_06_04_164922) do
     t.datetime "updated_at", null: false
     t.string "category"
     t.integer "volunteers"
-    t.integer "price_cents", default: 0, null: false
     t.integer "donation_cents", default: 0, null: false
   end
 
