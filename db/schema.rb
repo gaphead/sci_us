adc215c5119a55429f177e6b3deef12eb32955f8
Heather deManbey <h.demanbey@gmail.com>
2019-06-04 18:00:43 -0400

    made some changes to style on home, index and got the search and filter to work/be more easily clicked

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_31_141815) do
+ActiveRecord::Schema.define(version: 2019_06_03_192827) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -38,6 +38,8 @@ ActiveRecord::Schema.define(version: 2019_05_31_141815) do
     t.datetime "updated_at", null: false
     t.string "category"
     t.integer "volunteers"
+    t.integer "goal"
+    t.integer "campaign_duration"
   end
 
   create_table "users", force: :cascade do |t|
