d2662ce6677e53d5ada13dbadcb3e37cc9bbfeb3
Heather deManbey <h.demanbey@gmail.com>
2019-06-07 12:09:31 -0400

    css changes to show and profile

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -1,10 +1,13 @@
 class PaymentsController < ApplicationController
-  before_action :set_donation
+
 
   def new
   end
 
   def create
+    @project = Project.find(params[:project_id])
+    @donation = Donation.create(state: "pending", project: @project, amount: params[:amount].to_i, user: current_user)
+
     customer = Stripe::Customer.create(
       source: params[:stripeToken],
       email:  params[:stripeEmail]
@@ -17,19 +20,13 @@ class PaymentsController < ApplicationController
       currency:     @donation.amount.currency
     )
 
-    @donation.update(payment: charge.to_json, state: 'paid', amount: params[:amount])
+    @donation.update(payment: charge.to_json, state: 'paid')
     project = Project.find(@donation.project_id.to_i)
     project.current_funding += @donation.amount.to_i
     project.save!
 
     redirect_to donation_path(@donation), flash: { message: "You made your first donation!" }
   rescue Stripe::CardError => e
-    redirect_to new_donation_payment_path(@donation), alert: e.message
-  end
-
-  private
-
-  def set_donation
-    @donation = current_user.donations.where(state: 'pending').find(params[:donation_id])
+    redirect_to project_path(@project), alert: e.message
   end
 end
